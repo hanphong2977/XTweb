44c5d730486945b7934600fbc54b9983681e9289
bao nguyen <hanphong297@outlook.com>
2024-03-21 13:26:44 +0700

    Add project files.

---
+++ XTweb/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XTBarber.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult dichvu()
+        {
+            return View();
+
+        }
+
+        public ActionResult gioithieu()
+        {
+            return View();
+        }
+
+        public ActionResult lienhe() {
+
+            return View();
+        }
+
+        public ActionResult thanhtoan()
+        {
+            return View();
+        }
+
+        public ActionResult dangnhap()
+        {
+            return View();
+        }
+
+        public ActionResult dangky()
+        {
+            return View();
+        }
+
+        public ActionResult quenmatkhau() {
+            return View();
+        }
+
+        public ActionResult xacnhanemail()
+        {
+            return View();
+        }
+
+        public ActionResult matkhaumoi()
+        {
+            return View();
+        }
+    }
+}
