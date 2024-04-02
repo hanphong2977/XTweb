10a8997b337aca1c4cc909ee9683b0f454aefba1
bao nguyen <hanphong297@outlook.com>
2024-04-02 18:51:12 +0700

    commit all layout

--- XTweb/wwwroot/js/site.js
+++ XTweb/wwwroot/js/site.js
@@ -1,7 +1,9 @@
 ﻿const body = document.querySelector("body"),
     slidebar = body.querySelector(".slidebar"),
+    admin_nav = body.querySelector(".admin_nav"),
     toogle = body.querySelector(".toogle");
 
     toogle.addEventListener("click", () => {
         slidebar.classList.toggle("close");
+        admin_nav.classList.toggle("close");
     });
