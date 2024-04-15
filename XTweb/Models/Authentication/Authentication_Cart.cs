8f9f52136ce42aec647eccbc8b665a5b15a52d0a
HiroT0905 <163726665+HiroT0905@users.noreply.github.com>
2024-04-15 11:23:15 +0700

    New

---
+++ XTweb/Models/Authentication/Authentication_Cart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using XTweb.Extensions;
+
+namespace XTweb.Models.Authentication
+{
+    public class Authentication_Cart : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var tmp = context.HttpContext.Session.GetObjectFromJson<ShoppingCart>("Cart");
+            if (tmp == null)
+            {
+                context.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary()
+                    {
+                        {"Controller","GioHang"},
+                        {"Action","Index" }
+                    });
+            }
+
+        }
+    }
+}
